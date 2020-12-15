98af2b226ebd273f87da428a6c8c31dbba1b8c68
Jeroen Akkerman <Jeroen_akkerman1@hotmail.com>
2020-12-15 16:39:52 +0100

    Split test logic from test data

--- 5-gui-test/cypress/integration/purchase.spec.js
+++ 5-gui-test/cypress/integration/purchase.spec.js
@@ -9,23 +9,27 @@ describe('Purchase', () => {
     });
 
     it('can add an item to the basket', () => {
+
+        const product = 'Smartphone';
+        const productPrice = '100.00';
+
         // Log in
         cy.login(Cypress.config().user.email, Cypress.config().user.password);
 
         // Search for product
-        cy.get('.search-box form #small-searchterms').type('Smartphone');
+        cy.get('.search-box form #small-searchterms').type(product);
         cy.get('.search-box form').submit();
 
         // Check search results
-        cy.get('.search-results .product-item').first().contains('Smartphone');
+        cy.get('.search-results .product-item').first().contains(product);
 
         // Navigate to product page
         cy.get('.search-results .product-item img').first().click();
 
         // Check product information
         cy.url().should('eq', `${Cypress.config().baseUrl}/smartphone`);
-        cy.get('.product-essential .product-name').contains('Smartphone');
-        cy.get('.product-essential .product-price').contains('100.00');
+        cy.get('.product-essential .product-name').contains(product);
+        cy.get('.product-essential .product-price').contains(productPrice);
 
         // Add product to cart
         cy.get('.product-essential .add-to-cart-button').click();
@@ -38,12 +42,12 @@ describe('Purchase', () => {
         cy.get('.header-links #topcartlink a').click();
 
         // Check shopping cart information
-        cy.get('.cart .cart-item-row .product').first().contains('Smartphone');
-        cy.get('.cart .cart-item-row .product-unit-price').first().contains('100.00');
+        cy.get('.cart .cart-item-row .product').first().contains(product);
+        cy.get('.cart .cart-item-row .product-unit-price').first().contains(productPrice);
         cy.get('.cart .cart-item-row .qty input').first().should('have.value', 1);
-        cy.get('.cart .cart-item-row .subtotal').first().contains('100.00');
-        cy.get('.totals .cart-total .product-price').first().contains('100.00');
-        cy.get('.totals .cart-total .product-price.order-total').contains('100.00');
+        cy.get('.cart .cart-item-row .subtotal').first().contains(productPrice);
+        cy.get('.totals .cart-total .product-price').first().contains(productPrice);
+        cy.get('.totals .cart-total .product-price.order-total').contains(productPrice);
 
         // Remove product from cart
         cy.get('.cart .cart-item-row .remove-from-cart input').first().check();
