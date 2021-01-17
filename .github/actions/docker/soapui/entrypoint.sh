d39a81f75f0dbd7d1181095a39f539db5c1bb4e9
Thijs Calkhoven <t.calkhoven@live.nl>
2021-01-18 00:46:30 +0100

    Fixed if statement in entrypoint.sh

--- .github/actions/docker/soapui/entrypoint.sh
+++ .github/actions/docker/soapui/entrypoint.sh
@@ -9,8 +9,9 @@ if test -f "$GITHUB_WORKSPACE/$WORKING_DIR/$PROJECT_FILE"; then
 
   STATUS=`sh /opt/soapui/bin/testrunner.sh $* -f\"$GITHUB_WORKSPACE/$WORKING_DIR/TestResult\" $GITHUB_WORKSPACE/$WORKING_DIR/$PROJECT_FILE | grep 'Total Failed Assertions: *'`;
 
-if [ "$STATUS" != "Total Failed Assertions: 0" ]; then
-  return 103;
+  if [ "$STATUS" != "Total Failed Assertions: 0" ]; then
+    return 103;
+  fi
 else
   echo "Project file not found at $GITHUB_WORKSPACE/$WORKING_DIR/$PROJECT_FILE";
   return 1;
