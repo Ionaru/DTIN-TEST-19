d3dc8961adf121d08feff06851b039c5f49b41b9
Thijs Calkhoven <t.calkhoven@live.nl>
2021-01-17 23:35:59 +0100

    Add debug to correct file

--- .github/actions/docker/soapui/custom-testrunner.sh
+++ .github/actions/docker/soapui/custom-testrunner.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 
-STATUS=`sh /opt/soapui/bin/testrunner.sh "$@" | grep 'Total Failed Assertions: *'`;
+WORKING_DIR=$1; shift
+PROJECT_FILE=$1; shift
+echo `$GITHUB_WORKSPACE \
+  -f"$GITHUB_WORKSPACE/$WORKING_DIR/TestResult" \
+  "$GITHUB_WORKSPACE/$WORKING_DIR/$PROJECT_FILE" \
+  | grep 'Total Failed Assertions: *'`;
+# STATUS=`sh /opt/soapui/bin/testrunner.sh "$@" \
+#   -f"$GITHUB_WORKSPACE/$WORKING_DIR/TestResult" \
+#   "$GITHUB_WORKSPACE/$WORKING_DIR/$PROJECT_FILE" \
+#   | grep 'Total Failed Assertions: *'`;
 
-if [ "$STATUS" != "Total Failed Assertions: 0" ]; then
-  return 103;
-fi
+
+# if [ "$STATUS" != "Total Failed Assertions: 0" ]; then
+#   return 103;
+# fi
--- 2-api-test/scripts/custom-testrunner.sh
+++ 2-api-test/scripts/custom-testrunner.sh
@@ -1,17 +1,7 @@
 #!/bin/sh
 
-WORKING_DIR=$1; shift
-PROJECT_FILE=$1; shift
-echo `$GITHUB_WORKSPACE \
-  -f"$GITHUB_WORKSPACE/$WORKING_DIR/TestResult" \
-  "$GITHUB_WORKSPACE/$WORKING_DIR/$PROJECT_FILE" \
-  | grep 'Total Failed Assertions: *'`;
-# STATUS=`sh /opt/soapui/bin/testrunner.sh "$@" \
-#   -f"$GITHUB_WORKSPACE/$WORKING_DIR/TestResult" \
-#   "$GITHUB_WORKSPACE/$WORKING_DIR/$PROJECT_FILE" \
-#   | grep 'Total Failed Assertions: *'`;
+STATUS=`sh /opt/soapui/bin/testrunner.sh "$@" | grep 'Total Failed Assertions: *'`;
 
-
-# if [ "$STATUS" != "Total Failed Assertions: 0" ]; then
-#   return 103;
-# fi
+if [ "$STATUS" != "Total Failed Assertions: 0" ]; then
+  return 103;
+fi
