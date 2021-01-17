22658e736787fe7cead6db1e3fd53f11bb9e015b
Thijs Calkhoven <t.calkhoven@live.nl>
2021-01-18 00:17:30 +0100

    Add debug info

--- .github/actions/docker/soapui/entrypoint.sh
+++ .github/actions/docker/soapui/entrypoint.sh
@@ -6,6 +6,7 @@ PROJECT_FILE=$1; shift;
 echo "$GITHUB_WORKSPACE"
 echo "-f\"$GITHUB_WORKSPACE/$WORKING_DIR/TestResult\""
 echo "\"$GITHUB_WORKSPACE/$WORKING_DIR/$PROJECT_FILE\"";
+echo `ls $GITHUB_WORKSPACE/$WORKING_DIR`;
 # STATUS=`sh /opt/soapui/bin/testrunner.sh "$@" \
 #   -f"$GITHUB_WORKSPACE/$WORKING_DIR/TestResult" \
 #   "$GITHUB_WORKSPACE/$WORKING_DIR/$PROJECT_FILE" \
