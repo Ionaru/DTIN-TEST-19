5d96cd1c8b5d247b90d09df383ae811517db69cf
Thijs Calkhoven <t.calkhoven@live.nl>
2021-01-18 00:19:20 +0100

    Removed debug info

--- .github/actions/docker/soapui/entrypoint.sh
+++ .github/actions/docker/soapui/entrypoint.sh
@@ -1,18 +1,7 @@
 #!/bin/sh -l
 
+STATUS=`sh /opt/soapui/bin/testrunner.sh "$@" -f"$GITHUB_WORKSPACE/$WORKING_DIR/TestResult" "$GITHUB_WORKSPACE/$WORKING_DIR/$PROJECT_FILE" | grep 'Total Failed Assertions: *'`;
 
-WORKING_DIR=$1; shift;
-PROJECT_FILE=$1; shift;
-echo "$GITHUB_WORKSPACE"
-echo "-f\"$GITHUB_WORKSPACE/$WORKING_DIR/TestResult\""
-echo "\"$GITHUB_WORKSPACE/$WORKING_DIR/$PROJECT_FILE\"";
-echo `ls $GITHUB_WORKSPACE/$WORKING_DIR`;
-# STATUS=`sh /opt/soapui/bin/testrunner.sh "$@" \
-#   -f"$GITHUB_WORKSPACE/$WORKING_DIR/TestResult" \
-#   "$GITHUB_WORKSPACE/$WORKING_DIR/$PROJECT_FILE" \
-#   | grep 'Total Failed Assertions: *'`;
-
-
-# if [ "$STATUS" != "Total Failed Assertions: 0" ]; then
-#   return 103;
-# fi
+if [ "$STATUS" != "Total Failed Assertions: 0" ]; then
+  return 103;
+fi
