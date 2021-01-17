9e1ba5d9c07ea9629d76f034c3780c85c7c3cae8
Thijs Calkhoven <t.calkhoven@live.nl>
2021-01-17 23:28:01 +0100

    Add debug info

--- 2-api-test/scripts/custom-testrunner.sh
+++ 2-api-test/scripts/custom-testrunner.sh
@@ -2,10 +2,15 @@
 
 WORKING_DIR=$1; shift
 PROJECT_FILE=$1; shift
+echo `/opt/soapui/bin/testrunner.sh "$@" \
+  -f"$GITHUB_WORKSPACE/$WORKING_DIR/TestResult" \
+  "$GITHUB_WORKSPACE/$WORKING_DIR/$PROJECT_FILE" \
+  | grep 'Total Failed Assertions: *'`;
 STATUS=`sh /opt/soapui/bin/testrunner.sh "$@" \
   -f"$GITHUB_WORKSPACE/$WORKING_DIR/TestResult" \
   "$GITHUB_WORKSPACE/$WORKING_DIR/$PROJECT_FILE" \
   | grep 'Total Failed Assertions: *'`;
+
 
 if [ "$STATUS" != "Total Failed Assertions: 0" ]; then
   return 103;
