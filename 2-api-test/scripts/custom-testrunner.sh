b2066f273b3a33be91690a18259997eb0b8c7570
Thijs Calkhoven <t.calkhoven@live.nl>
2021-01-17 23:25:10 +0100

    Moved Github Workspace into entrypoint script

--- 2-api-test/scripts/custom-testrunner.sh
+++ 2-api-test/scripts/custom-testrunner.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-STATUS=`sh /opt/soapui/bin/testrunner.sh "$@" | grep 'Total Failed Assertions: *'`;
+WORKING_DIR=$1; shift
+PROJECT_FILE=$1; shift
+STATUS=`sh /opt/soapui/bin/testrunner.sh "$@" \
+  -f"$GITHUB_WORKSPACE/$WORKING_DIR/TestResult" \
+  "$GITHUB_WORKSPACE/$WORKING_DIR/$PROJECT_FILE" \
+  | grep 'Total Failed Assertions: *'`;
 
 if [ "$STATUS" != "Total Failed Assertions: 0" ]; then
   return 103;
