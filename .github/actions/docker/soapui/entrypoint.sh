72569ef5c0c8be69fbd9fe183204460c8d65617a
Thijs Calkhoven <t.calkhoven@live.nl>
2021-01-18 00:23:24 +0100

    Fixed argument shifting in entrypoint

--- .github/actions/docker/soapui/entrypoint.sh
+++ .github/actions/docker/soapui/entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/sh -l
 
+
+WORKING_DIR=$1; shift;
+PROJECT_FILE=$1; shift;
+
+echo "Running testrunner.sh with testrunner.sh \"$@\" -f\"$GITHUB_WORKSPACE/$WORKING_DIR/TestResult\" \"$GITHUB_WORKSPACE/$WORKING_DIR/$PROJECT_FILE\"";
+echo `test -f "$GITHUB_WORKSPACE/$WORKING_DIR/$PROJECT_FILE"`;
+
 STATUS=`sh /opt/soapui/bin/testrunner.sh "$@" -f"$GITHUB_WORKSPACE/$WORKING_DIR/TestResult" "$GITHUB_WORKSPACE/$WORKING_DIR/$PROJECT_FILE" | grep 'Total Failed Assertions: *'`;
 
 if [ "$STATUS" != "Total Failed Assertions: 0" ]; then
