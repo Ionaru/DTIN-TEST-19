8c4f636add1ac717b58c64839bf824308f11805e
Thijs Calkhoven <t.calkhoven@live.nl>
2021-01-18 00:44:41 +0100

    Fixed syntax error in entrypoint

--- .github/actions/docker/soapui/entrypoint.sh
+++ .github/actions/docker/soapui/entrypoint.sh
@@ -5,13 +5,12 @@ PROJECT_FILE=$1; shift;
 
 if test -f "$GITHUB_WORKSPACE/$WORKING_DIR/$PROJECT_FILE"; then
   echo "Project file exists.";
-  echo "testrunner.sh $* -f\"$GITHUB_WORKSPACE/$WORKING_DIR/TestResult\" $GITHUB_WORKSPACE/$WORKING_DIR/$PROJECT_FILE;
+  echo "testrunner.sh $* -f\"$GITHUB_WORKSPACE/$WORKING_DIR/TestResult\" $GITHUB_WORKSPACE/$WORKING_DIR/$PROJECT_FILE";
 
   STATUS=`sh /opt/soapui/bin/testrunner.sh $* -f\"$GITHUB_WORKSPACE/$WORKING_DIR/TestResult\" $GITHUB_WORKSPACE/$WORKING_DIR/$PROJECT_FILE | grep 'Total Failed Assertions: *'`;
 
-  if [ "$STATUS" != "Total Failed Assertions: 0" ]; then
-    return 103;
-  fi
+if [ "$STATUS" != "Total Failed Assertions: 0" ]; then
+  return 103;
 else
   echo "Project file not found at $GITHUB_WORKSPACE/$WORKING_DIR/$PROJECT_FILE";
   return 1;
