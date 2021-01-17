fdae63003fab20f586e41bb7176a8a51cc5a65f1
Thijs Calkhoven <t.calkhoven@live.nl>
2021-01-17 23:43:48 +0100

    Add debug

--- .github/actions/docker/soapui/custom-testrunner.sh
+++ .github/actions/docker/soapui/custom-testrunner.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
+echo $1
 echo $@;
 echo $#;
-WORKING_DIR=$1; shift;
-PROJECT_FILE=$1; shift;
-echo `$GITHUB_WORKSPACE -f"$GITHUB_WORKSPACE/$WORKING_DIR/TestResult" "$GITHUB_WORKSPACE/$WORKING_DIR/$PROJECT_FILE" | grep 'Total Failed Assertions: *'`;
+# WORKING_DIR=$1; shift;
+# PROJECT_FILE=$1; shift;
+# echo `$GITHUB_WORKSPACE -f"$GITHUB_WORKSPACE/$WORKING_DIR/TestResult" "$GITHUB_WORKSPACE/$WORKING_DIR/$PROJECT_FILE" | grep 'Total Failed Assertions: *'`;
 # STATUS=`sh /opt/soapui/bin/testrunner.sh "$@" \
 #   -f"$GITHUB_WORKSPACE/$WORKING_DIR/TestResult" \
 #   "$GITHUB_WORKSPACE/$WORKING_DIR/$PROJECT_FILE" \
