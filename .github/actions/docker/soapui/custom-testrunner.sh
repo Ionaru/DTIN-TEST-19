6bf6591ed482449381f0207369bd0c892798ee49
Thijs Calkhoven <t.calkhoven@live.nl>
2021-01-17 23:40:03 +0100

    Added more debug to correct file

--- .github/actions/docker/soapui/custom-testrunner.sh
+++ .github/actions/docker/soapui/custom-testrunner.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 
-WORKING_DIR=$1; shift
-PROJECT_FILE=$1; shift
-echo `$GITHUB_WORKSPACE \
-  -f"$GITHUB_WORKSPACE/$WORKING_DIR/TestResult" \
-  "$GITHUB_WORKSPACE/$WORKING_DIR/$PROJECT_FILE" \
-  | grep 'Total Failed Assertions: *'`;
+echo $#;
+WORKING_DIR=$1; shift;
+PROJECT_FILE=$1; shift;
+echo `$GITHUB_WORKSPACE -f"$GITHUB_WORKSPACE/$WORKING_DIR/TestResult" "$GITHUB_WORKSPACE/$WORKING_DIR/$PROJECT_FILE" | grep 'Total Failed Assertions: *'`;
 # STATUS=`sh /opt/soapui/bin/testrunner.sh "$@" \
 #   -f"$GITHUB_WORKSPACE/$WORKING_DIR/TestResult" \
 #   "$GITHUB_WORKSPACE/$WORKING_DIR/$PROJECT_FILE" \
