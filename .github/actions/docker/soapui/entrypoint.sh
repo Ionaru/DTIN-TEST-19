a9c34a0dbaf945dc855c32e8e104c5e060bb52f5
Thijs Calkhoven <t.calkhoven@live.nl>
2021-01-18 01:00:55 +0100

    Removed CLI options from action.yml

--- .github/actions/docker/soapui/entrypoint.sh
+++ .github/actions/docker/soapui/entrypoint.sh
@@ -2,12 +2,13 @@
 
 WORKING_DIR=$1; shift;
 PROJECT_FILE=$1; shift;
+TEST_SUITE=$1; shift;
 
 if test -f "$GITHUB_WORKSPACE/$WORKING_DIR/$PROJECT_FILE"; then
   echo "Project file exists.";
-  echo "testrunner.sh $* -f$GITHUB_WORKSPACE/$WORKING_DIR/TestResult \"$GITHUB_WORKSPACE/$WORKING_DIR/$PROJECT_FILE\"";
+  echo "testrunner.sh -s"$TEST_SUITE" -r -j -f$GITHUB_WORKSPACE/$WORKING_DIR/TestResult -I $GITHUB_WORKSPACE/$WORKING_DIR/$PROJECT_FILE";
 
-  STATUS=`sh /opt/soapui/bin/testrunner.sh $* -f$GITHUB_WORKSPACE/$WORKING_DIR/TestResult "$GITHUB_WORKSPACE/$WORKING_DIR/$PROJECT_FILE" | grep 'Total Failed Assertions: *'`;
+  STATUS=`sh /opt/soapui/bin/testrunner.sh -s"$TEST_SUITE" -r -j -f$GITHUB_WORKSPACE/$WORKING_DIR/TestResult -I $GITHUB_WORKSPACE/$WORKING_DIR/$PROJECT_FILE | grep 'Total Failed Assertions: *'`;
 
   if [ "$STATUS" != "Total Failed Assertions: 0" ]; then
     return 103;
