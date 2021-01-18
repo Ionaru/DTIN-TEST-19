d629747f8b939e55007c0cc14336b9a34f1a1d60
Thijs Calkhoven <t.calkhoven@live.nl>
2021-01-18 01:15:44 +0100

    Add xmlbeans override

--- .github/actions/docker/soapui/entrypoint.sh
+++ .github/actions/docker/soapui/entrypoint.sh
@@ -6,7 +6,7 @@ TEST_SUITE=$1; shift;
 
 if test -f "$GITHUB_WORKSPACE/$WORKING_DIR/$PROJECT_FILE"; then
   echo "Project file exists.";
-  echo "testrunner.sh -s"$TEST_SUITE" -r -j -f$GITHUB_WORKSPACE/$WORKING_DIR/TestResult -I $GITHUB_WORKSPACE/$WORKING_DIR/$PROJECT_FILE";
+  echo "testrunner.sh -s"$TEST_SUITE" -r -j -f$GITHUB_WORKSPACE/$WORKING_DIR/TestResult -I $GITHUB_WORKSPACE/$WORKING_DIR/$PROJECT_FILE ";
 
   STATUS=`sh /opt/soapui/bin/testrunner.sh -s"$TEST_SUITE" -r -j -f$GITHUB_WORKSPACE/$WORKING_DIR/TestResult -I $GITHUB_WORKSPACE/$WORKING_DIR/$PROJECT_FILE | grep 'Total Failed Assertions: *'`;
 
